3da8cfa35d3788b8cfea698c58302cb9d6f993de
Stas Morozevich <stas.morozevich@gmail.com>
2019-08-02 01:46:21 +0300

    Update eslint config and add lefthook

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 // NOTE: please import only global styles here. If you want to have local styles please use css-modules
 // Check components/HelloWorld as an example
 
-import "./src/styles/reset.css"
-import "./src/styles/global.css"
+import './src/styles/reset.css';
+import './src/styles/global.css';
