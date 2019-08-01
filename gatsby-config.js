759354d4e3a30c965b2bfd02393409ff2e0fc1dd
Stas Morozevich <stas.morozevich@gmail.com>
2019-08-02 01:34:49 +0300

    Add an example of collection page with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,18 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const path = require('path');
 
 module.exports = {
-  /* Your site config here */
-}
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+  ],
+};
