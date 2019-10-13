4e4b4644c3cecdccbb8d170da24a7be0095aa80f
Stas Morozevich <stas.morozevich@gmail.com>
2019-10-13 22:20:35 +0300

    Implement basic slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import ColumnLayout from '../components/ColumnLayout';
+import Carousel from 'nuka-carousel';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+const SliderDataQuery = graphql`
+  query SliderData {
+    sliderImages: allFile(filter: { relativeDirectory: { eq: "slider" } }) {
+      edges {
+        node {
+          childImageSharp {
+            id
+            fixed(width: 315) {
+              ...GatsbyImageSharpFixed_noBase64
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Slider = ({ images }) => {
+  console.log(images);
+  return (
+    <Carousel slidesToShow={3} enableKeyboardControls>
+      {images.map(({ fixed, id }) => (
+        <div key={id}>
+          <Img fixed={fixed} alt="Slider image" />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
 
 const IndexPage = () => {
-  const leftColumn = <h1>hello leftColumn</h1>;
-  const rightColumn = <h1>hello rightColumn</h1>;
-
+  const {
+    sliderImages: { edges },
+  } = useStaticQuery(SliderDataQuery);
+  const images = edges.map(({ node: { childImageSharp } }) => childImageSharp);
+  console.log('images', images);
   return (
     <Layout>
       <h1>Start page</h1>
-      <ColumnLayout leftColumn={leftColumn} rightColumn={rightColumn} />
+      <Slider images={images} />
     </Layout>
   );
 };
