6ed63d0f6462063a6ec651b6b1a0da78bd701ded
Stas Morozevich <stas.morozevich@gmail.com>
2019-11-03 13:26:30 +0300

    Add about us and contacts

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import ColumnLayout from '../components/ColumnLayout';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styles from '../components/ColumnLayout/ColumnLayout.module.css';
 
-const AboutUs = () => {
+const AboutUs = ({ data: { aboutUsPicture } }) => {
+  console.log(aboutUsPicture);
   return (
     <Layout>
-      <p>Страница О нас</p>
+      <ColumnLayout
+        leftColumn={
+          <aside className={styles.aside}>
+            <Img
+              fixed={aboutUsPicture.childImageSharp.fixed}
+              alt="Фотография женщины в платье"
+            ></Img>
+          </aside>
+        }
+        rightColumn={
+          <section>
+            <h2>О нас</h2>
+            <p>
+              Мы - это фирма по разработке и производству женской одежды,
+              динамично развивающаяся, но уже имеющая свои традиции, узнаваемый
+              стиль.
+            </p>
+            <p>
+              С момента образования в 2005 году мы сумели приобрести истинных
+              почитателей нашей продукции, как в Беларуси, так и за её пределами
+              - в России, Украине. Что же нравится в нашей продукции
+              потребителям.
+            </p>
+          </section>
+        }
+      />
     </Layout>
   );
 };
+
+export const query = graphql`
+  query AboutUsQuery {
+    aboutUsPicture: file(relativePath: { eq: "other/random3.jpg" }) {
+      childImageSharp {
+        id
+        fixed(width: 315) {
+          ...GatsbyImageSharpFixed_noBase64
+        }
+      }
+    }
+  }
+`;
 
 export default AboutUs;
--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import ColumnLayout from '../components/ColumnLayout';
+import styles from '../components/ColumnLayout/ColumnLayout.module.css';
 
 const Contacts = () => {
   return (
     <Layout>
-      <p>Страница контактов</p>
+      <ColumnLayout
+        leftColumn={
+          <aside className={styles.aside}>
+            <h2>Почтовый адрес:</h2>
+            <ul>
+              <li>224011 г.Брест</li>
+              <li>ул. Луцкая, 46</li>
+              <li>Республика Беларусь</li>
+            </ul>
+            <iframe
+              title="map"
+              src="https://yandex.com/map-widget/v1/?um=constructor%3Aa7d0d85cd0c4dafa1df9bc1bf981bbcdb849de4d41f142656d826c8931dd1ba4&amp;source=constructor"
+              width="403"
+              height="396"
+              frameBorder="0"
+            ></iframe>
+          </aside>
+        }
+        rightColumn={
+          <section className={styles.section}>
+            <h2>Контактные телефоны:</h2>
+            <ul>
+              <li>
+                <span>т/ф:</span>{' '}
+                <strong>
+                  <a href="tel:+375162486022">+375 (162) 48-60-22</a>
+                </strong>
+              </li>
+              <li>
+                <span>Директор:</span>{' '}
+                <strong>
+                  <a href="tel:+375292238282">+375 (29) 223-82-82</a>
+                </strong>
+              </li>
+              <li>
+                <span>Бухгалтер:</span> {/* TODO: уточнить номер */}
+                <strong>
+                  <a href="tel:">+375 (162) 52-04-</a>
+                </strong>
+              </li>
+              <li>
+                <span>email:</span>{' '}
+                <strong>
+                  <a href="mailto:ivabrest@mail.ru">ivabrest@mail.ru</a>
+                </strong>
+              </li>
+            </ul>
+          </section>
+        }
+      />
     </Layout>
   );
 };
